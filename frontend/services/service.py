12bdcdfa49153c7f4f4360f4219e1a2875279e02
Daniel Luque <daniel.luquegallardo@gmail.com>
2026-06-24 09:41:47 +0200

    feat(frontend): Form predicction ad model selection method added

--- frontend/services/service.py
+++ frontend/services/service.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-URL_BACKEND_INTERSHIPS = os.getenv('URL_BACKEND_INTERSHIPS')
+URL_BACKEND_INTERNSHIPS = os.getenv('URL_BACKEND_INTERNSHIPS')
 
 def get_intership_data():
     try:
-        response = requests.get(URL_BACKEND_INTERSHIPS)
+        response = requests.get(URL_BACKEND_INTERNSHIPS)
         if response.status_code == 200:
             return response.json()
         else:
